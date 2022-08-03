adb0cd223843e251eb48b72c65b63a6d2717cf1e
André Luis Fernandes <62942316+Andre-StudioNerd@users.noreply.github.com>
2022-08-03 15:10:45 -0300

    Update conecta.php

--- includes/conecta.php
+++ includes/conecta.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "br384.hostgator.com.br";
-$database = "studi092_reserva";
-$username = "studi092_root";
-$password = "melina80";
+$database = "studi_reserva";
+$username = "studi_root";
+$password = "bruna80";
 // Cria conexão
 $conn = mysqli_connect($servername, $username, $password, $database);
 // Checka conexão
