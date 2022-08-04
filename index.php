0aa75a0e82d4b30c8584e3ce8e357b7f5b9115da
André Luis Fernandes <62942316+Andre-StudioNerd@users.noreply.github.com>
2022-08-04 12:34:24 -0300

    Update index.php

--- index.php
+++ index.php
@@ -35,10 +35,7 @@ session_start();
 
 
    <!-- Jquery Arquivo-->
-   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery.maskedinput/1.4.1/jquery.maskedinput.min.js"></script>
    
-   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
 
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
